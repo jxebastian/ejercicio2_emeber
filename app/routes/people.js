d7158dd5337aaf9c0e0c318a37a39b5a74657ba4
jxebastian <josrodriguezca@unal.edu.co>
2018-03-16 17:34:41 -0500

    elimine las personas que habia creado

--- app/routes/people.js
+++ app/routes/people.js
@@ -1,22 +1,5 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-  model(){
-    return {
-      people : [
-        {
-          username: 'cristian',
-          password: '12345',
-        },
-        {
-          username: 'lucas',
-          password: '123456',
-        },
-        {
-          username: 'mateo',
-          password: '1234',
-        },
-      ],
-    };
-  },
+  
 });
