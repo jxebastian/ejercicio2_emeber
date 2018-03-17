fc7bead9d483dedffd40e0a16a738863f8b3962d
jxebastian <josrodriguezca@unal.edu.co>
2018-03-17 12:29:25 -0500

    Validacion incompleta

--- app/controllers/register.js
+++ app/controllers/register.js
@@ -8,11 +8,19 @@ export default Controller.extend({
       const name = $('#name').val();
       const sex = $('#sex').val();
       const salary = $('#salary').val();
-      const json = {name, sex, salary};
-      this.get('people').add(json);
-      $('#name').val('');
-      $('#sex').val('');
-      $('#salary').val('');
+      if(name==='' || sex==='' || salary===''){
+        $('.msg2')
+        .addClass("alert alert-danger")
+        .append('<span class="glyphicon glyphicon-exclamation-sign"></span> Llene todos los campos');
+        //$('#msg').append('<hr>');
+        console.log("mal");
+      }else{
+        const json = {name, sex, salary};
+        this.get('people').add(json);
+        $('#name').val('');
+        $('#sex').val('');
+        $('#salary').val('');
+      }
     },
   },
 });
--- app/routes/login.js
+++ app/routes/login.js
@@ -4,14 +4,8 @@ import { inject as service } from '@ember/service';
 export default Route.extend({
   car: service('login'),
   model(){
-<<<<<<< HEAD
-    users: service('login');
-    console.log(this.get('users'));
-    if(this.get('car')){
-=======
     // console.log(this.get('car').items[0]);
-    if(this.get('car').items.length > 0){
->>>>>>> ad648cddbc5116d4d0e935496953fa5264acdc93
+    if(this.get('car').size() > 0){
       this.transitionTo('index');
     }
   }
