18f3bddb25e6e80d678ad117d9cd101820b536d7
lucasoz <luemunozmo@unal.edu.co>
2018-03-17 00:02:39 -0500

    Se realizo el registro y listado de personas usando el servicio

---
+++ app/controllers/register.js
@@ -0,0 +1,16 @@
+import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
+
+export default Controller.extend({
+  people: service('person-service'),
+  actions: {
+    register(){
+      const name = $('#name').val();
+      const sex = $('#sex').val();
+      const salary = $('#salary').val();
+
+      const json = {name, sex, salary};
+      this.get('people').add(json);
+    },
+  },
+});
