000b61f4812d26da3b64b85d9515efd22388cc5f
jxebastian <josrodriguezca@unal.edu.co>
2018-03-17 13:39:50 -0500

    Literal d completo

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,12 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default Route.extend({
+  car: service('login'),
+  model(){
+    // console.log(this.get('car').items[0]);
+    if(this.get('car').items.length == 0){
+       this.transitionTo('login');
+    }
+  }
 });
